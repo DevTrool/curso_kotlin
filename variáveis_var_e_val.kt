b2d33d2ad05ce165cb6292413b3d3a7231e07cc0
Gabriel Rosa <89321808+DevTrool@users.noreply.github.com>
2024-03-16 20:31:16 -0300

    Create variáveis_var_e_val.kt

---
+++ variáveis_var_e_val.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    var a: String = "Inicial"  //posso mudar
+    println(a)
+    a = "final" //por ser uma val eu posso alterar 
+    println(a)
+
+    val b: Int = 1      //nao posso mudar | estou falando o tipo
+    val c = 3      //nao posso mudar | deixando o kt descobrir
+    println(b)
+    println(c)
+
+    //var eu posso mudar | já o val eu não posso mudar !
+    
+}
